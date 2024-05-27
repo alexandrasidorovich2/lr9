8a5947b7559f27e98c5cfbf9139e4aeb1678103e
alexandrasidorovich2 <sidorovich2sasha@gmail.com>
2024-05-27 17:11:22 +0300

    Добавлен вывод общей информации о задании

---
+++ main-project/main-project/main-project.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Laboratory work #9. GIT\n";
+    cout << "Variant #10. Каталог товаров\n";
+    cout << "Author: Сидорович Александра\n";
+    return 0;
+}
